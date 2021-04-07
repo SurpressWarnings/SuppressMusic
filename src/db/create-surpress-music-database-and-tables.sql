6c18cb13ca2f32778e7ec470cd7546a4aeeb10e5
mjtillman <mjtillman@gmail.com>
2021-04-07 13:50:04 -0500

    took DEFAULT out of script

--- src/db/create-surpress-music-database-and-tables.sql
+++ src/db/create-surpress-music-database-and-tables.sql
@@ -2,43 +2,43 @@ CREATE DATABASE IF NOT EXISTS surpress_music;
 
 CREATE TABLE IF NOT EXISTS albums (
   album_id int NOT NULL AUTO_INCREMENT,
-  album varchar(255) DEFAULT NOT NULL,
-  genre varchar(255) DEFAULT NOT NULL,
+  album varchar(255) NOT NULL,
+  genre varchar(255) NOT NULL,
   PRIMARY KEY (album_id)
 );
 
  CREATE TABLE IF NOT EXISTS songs (
   song_id int NOT NULL AUTO_INCREMENT,
-  artist varchar(255) DEFAULT NOT NULL,
-  format varchar(255) DEFAULT NULL,
-  genre varchar(255) DEFAULT NULL,
-  price float DEFAULT NOT NULL,
-  song varchar(255) DEFAULT NOT NULL,
-  album_id int DEFAULT NULL,
+  artist varchar(255) NOT NULL,
+  format varchar(255) NULL,
+  genre varchar(255) NULL,
+  price float NOT NULL,
+  song varchar(255) NOT NULL,
+  album_id int NULL,
   PRIMARY KEY (song_id),
   FOREIGN KEY(album_id) REFERENCES albums(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS genres (
   genre_id int NOT NULL AUTO_INCREMENT,
-  genre VARCHAR(255) DEFAULT NOT NULL,
-  display_name VARCHAR(255) DEFAULT NOT NULL,
+  genre VARCHAR(255) NOT NULL,
+  display_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (genre_id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
   user_id int NOT NULL AUTO_INCREMENT,
-  cc_number varchar(255) DEFAULT NULL,
-  first_name varchar(255) DEFAULT NOT NULL,
-  last_name varchar(255) DEFAULT NOT NULL,
-  password varchar(255) DEFAULT NOT NULL,
-  user_name varchar(255) DEFAULT NOT NULL,
+  cc_number varchar(255) NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  user_name varchar(255) NOT NULL,
   PRIMARY KEY (user_id)
 );
 
 CREATE TABLE  IF NOT EXISTS roles (
   role_id int NOT NULL AUTO_INCREMENT,
-  role varchar(255) DEFAULT NOT NULL,
+  role varchar(255) NOT NULL,
   PRIMARY KEY (role_id)
 )
 
