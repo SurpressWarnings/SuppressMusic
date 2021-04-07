ae476547685b058c6b6b4e3e93412538803cfc83
mjtillman <mjtillman@gmail.com>
2021-04-07 11:57:34 -0500

    Got views with thymeleaf to work

--- src/main/java/com/surpressmusic/store/security/WebSecurityConfig.java
+++ src/main/java/com/surpressmusic/store/security/WebSecurityConfig.java
@@ -1,70 +1,78 @@
-package com.surpressmusic.store.security;
-
-import com.surpressmusic.store.services.TheUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
-	@Autowired
-	TheUserDetailsService userDetailsService;
-	
-	@Autowired 
-	BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		auth.userDetailsService(userDetailsService)
-				.passwordEncoder(tempPasswordEncoder());
-				//.passwordEncoder(passwordEncoder());
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		http.authorizeRequests()
-				.antMatchers("/admin").hasRole("ADMIN")
-				.antMatchers("/", "/register", "/css/*", "/images/*").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				   .formLogin()
-		            .loginPage("/login")
-		            .defaultSuccessUrl("/mainmenu")
-		            .failureUrl("/login?error=true")
-		            .permitAll()
-		        .and()
-		            .logout()
-		            .logoutSuccessUrl("/")
-		            .invalidateHttpSession(true)
-		            .permitAll()
-		        .and()
-		            .csrf()
-		            .disable();
-	}
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() 
-	{
-		return new BCryptPasswordEncoder();
-	}
-	
-	
-	// This is a work-around to store as passwords as plain text
-	// THIS SHOULD NOT MAKE IT TO PRODUCTION - DELETE BEFORE SUBMITTING
-	@Bean
-	public PasswordEncoder tempPasswordEncoder()
-	{
-		return NoOpPasswordEncoder.getInstance();
-	}
-}
+//package com.surpressmusic.store.security;
+//
+//import com.surpressmusic.store.services.TheUserDetailsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//
+//	@Autowired
+//	TheUserDetailsService userDetailsService;
+//
+//	@Autowired
+//	BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		// TODO Auto-generated method stub
+//		auth.userDetailsService(userDetailsService)
+//				.passwordEncoder(tempPasswordEncoder());
+//				//.passwordEncoder(passwordEncoder());
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		// TODO Auto-generated method stub
+//		http.authorizeRequests()
+//				.antMatchers("/admin").hasRole("ADMIN")
+//				.antMatchers("/", "/register").permitAll()
+//				.anyRequest().authenticated()
+//				.and()
+//				   .formLogin()
+//		            .loginPage("/login")
+//		            .defaultSuccessUrl("/mainmenu")
+//		            .failureUrl("/login?error=true")
+//		            .permitAll()
+//		        .and()
+//		            .logout()
+//		            .logoutSuccessUrl("/")
+//		            .invalidateHttpSession(true)
+//		            .permitAll()
+//		        .and()
+//		            .csrf()
+//		            .disable();
+//	}
+//
+//	@Override
+//	public void configure(WebSecurity web) throws Exception {
+//		web
+//				.ignoring()
+//				.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+//	}
+//
+//	@Bean
+//	public BCryptPasswordEncoder passwordEncoder()
+//	{
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//
+//	// This is a work-around to store as passwords as plain text
+//	// THIS SHOULD NOT MAKE IT TO PRODUCTION - DELETE BEFORE SUBMITTING
+//	@Bean
+//	public PasswordEncoder tempPasswordEncoder()
+//	{
+//		return NoOpPasswordEncoder.getInstance();
+//	}
+//}
