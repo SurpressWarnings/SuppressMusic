9584cf724d16537eac8964248912ef9a1262815a
Anmol Mandara <anmolmandara1@gmail.com>
2021-04-08 11:53:22 -0500

    Connected register to database for adding new users

--- src/main/java/com/surpressmusic/store/controllers/HomeController.java
+++ src/main/java/com/surpressmusic/store/controllers/HomeController.java
@@ -1,10 +1,21 @@
 package com.surpressmusic.store.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.surpressmusic.store.model.User;
+import com.surpressmusic.store.services.UserService;
+
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
 
 
    @GetMapping({"/", "index"})
@@ -22,6 +33,18 @@ public class HomeController {
 	   return "register";
    }
    
+   @PostMapping("/register")
+   public String addUsers(ModelMap model, @RequestParam String firstname, @RequestParam String lastname, @RequestParam String username, 
+   		@RequestParam String psw) {
+   	User u = new User();
+   	u.setFirstName(firstname);
+   	u.setLastName(lastname);
+   	u.setUsername(username);
+   	u.setPassword(psw);
+   	model.addAttribute("user", u);
+   	userService.saveUser(u);
+   	return "usersuccess";
+   }  
    
 
 }
