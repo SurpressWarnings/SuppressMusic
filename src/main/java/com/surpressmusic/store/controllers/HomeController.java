10c5e9a2a685100077b53d1cacf66e82204291aa
Kenny Hoang <43805048+kennykthoang@users.noreply.github.com>
2021-04-11 17:49:56 -0500

    Added Logout function and base for admin page

--- src/main/java/com/surpressmusic/store/controllers/HomeController.java
+++ src/main/java/com/surpressmusic/store/controllers/HomeController.java
@@ -2,6 +2,9 @@ package com.surpressmusic.store.controllers;
 
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,9 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 import com.surpressmusic.store.model.User;
-import com.surpressmusic.store.model.TheUserDetails;
 import com.surpressmusic.store.services.UserService;
 
 
@@ -81,6 +84,14 @@ public class HomeController {
 	   	return "login";
    }  
    
+   @GetMapping("/logout")
+   public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+       if (auth != null){    
+           new SecurityContextLogoutHandler().logout(request, response, auth);
+       }
+       return "/";
+   }
    
    @GetMapping("/userdetails")
    public String userdetails() {
