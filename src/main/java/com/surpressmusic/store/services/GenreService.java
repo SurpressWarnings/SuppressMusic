c11559773db2f40dd138dc5e214994e0db69c2c1
Kenny Hoang <43805048+kennykthoang@users.noreply.github.com>
2021-04-11 22:54:49 -0500

    Added Admin function - Add Genres

--- src/main/java/com/surpressmusic/store/services/GenreService.java
+++ src/main/java/com/surpressmusic/store/services/GenreService.java
@@ -1,6 +1,8 @@
 package com.surpressmusic.store.services;
 
+import com.surpressmusic.store.exceptions.UserNotFoundException;
 import com.surpressmusic.store.model.Genre;
+import com.surpressmusic.store.model.User;
 import com.surpressmusic.store.repositories.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,4 +22,16 @@ public class GenreService {
 
    public Optional<Genre> getById(Integer id) {
       return genreRepo.findById(id); }
+   
+   public Genre getUserByGenreType(String genre){
+	      Optional<Genre> foundGenre = genreRepo.findByGenreType(genre);
+
+	      return foundGenre.orElseGet(Genre::new);
+	   }
+   
+   public void updateGenre(Genre genre)
+   {
+	   genreRepo.save(genre);
+   }
 }
+
