8233ecae523db760afa1177d44b1086ba65cf294
Kenny Hoang <43805048+kennykthoang@users.noreply.github.com>
2021-04-11 22:24:16 -0500

    Fixed order of antMatchers to correctly deny unauthorized access

--- src/main/java/com/surpressmusic/store/security/WebSecurityConfig.java
+++ src/main/java/com/surpressmusic/store/security/WebSecurityConfig.java
@@ -32,9 +32,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.authorizeRequests()
-				.antMatchers("/", "/register").permitAll()
 				.antMatchers("/admin/**").hasRole("ADMIN")
 				.antMatchers("/shoppingcart").hasRole("USER")
+				.antMatchers("/", "/register").permitAll()
 				.and()
 				   .formLogin()
 		            .loginPage("/login")
